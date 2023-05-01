4ac35d0059e635720911ed6ea38715c75a10a030
Mateusz <matekumateku@gmail.com>
2023-05-01 10:35:21 +0200

    my solution

--- app.js
+++ app.js
@@ -3,3 +3,13 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+const linksUl = document.querySelector(".links");
+const navBtn = document.querySelector(".nav-toggle");
+
+navBtn.addEventListener("click", () => {
+  if (!linksUl.classList.contains("show-links")) {
+    linksUl.classList.add("show-links");
+  } else {
+    linksUl.classList.remove("show-links");
+  }
+});
